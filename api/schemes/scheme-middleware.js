a222e6449e73b5761b061ba69e2162903c20114a
CaitlinTalbert <92820628+CaitlinTalbert@users.noreply.github.com>
2022-04-20 14:51:11 -0500

    wrote some middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const db = require("../../data/db-config");
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +7,23 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
-}
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const scheme_id = await db("schemes")
+      .where("scheme_id", req.params.scheme_id)
+      .first();
+    if (!scheme_id) {
+      next({
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`,
+        status: 404,
+      });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +33,22 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = async (req, res, next) => {
+  try {
+    const { scheme_name } = req.body;
+    if (
+      scheme_name === undefined ||
+      typeof scheme_name !== "string" ||
+      !scheme_name.trim()
+    ) {
+      next({ status: 400, message: "invalid scheme_name" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +59,27 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = async (req, res, next) => {
+  try {
+    const { instructions, step_number } = req.body;
+    if (
+      instructions === undefined ||
+      typeof instructions !== "string" ||
+      !instructions.trim() ||
+      typeof step_number !== "number" ||
+      step_number < 1
+    ) {
+      next({ status: 400, message: "invalid step" });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
